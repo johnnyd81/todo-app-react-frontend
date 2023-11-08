03627fa122f1893651a40f9f803732f534345bdb
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2023-11-08 20:52:10 +0200

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,7 +8,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+//by using the trim method trailing whitespace is removed from the username and the password
     await signup(username.trim(), password.trim());
   };
 
